f206ec0718b8aaf7a10afbe0bc28f8771ef04308
Shady Diaa <ShadyDiaa2014@gmail.com>
2020-06-07 11:29:07 +0200

    Adding definations of function and implementation

--- stats.c
+++ stats.c
@@ -34,36 +34,108 @@ void main() {
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-
+  
+  printf("Array Elements : \n");
+  print_array(test,sizeof(test));
+  printf("*************************\n");
+  printf("Statistics : \n");
+  print_statistics(test,sizeof(test));
+  printf("*************************\n");
+  printf("Array Elements after sorted descending:\n");
+  print_array(test,sizeof(test));
 }
 
 
 void print_statistics(unsigned char* arr,unsigned int length)
 {
+	printf("Min : %d\n",find_minimum(arr,length));
+	printf("Max : %d\n",find_maximum(arr,length));
+	printf("Mean : %d\n",find_mean(arr,length));
+	printf("Median : %d\n",find_median(arr,length));
 }
 void print_array(unsigned char* arr,unsigned int length)
 {
+	unsigned char counter; 
+	for(counter=0;counter<length;counter++)
+	{
+		printf("arr[%d]=%d\n",counter,arr[counter]);
+	}
 }
 unsigned char find_median(unsigned char* arr,unsigned int length)
 {
+	sort_array(arr,length);
+	//even 
+	if(length%2==0)
+	{
+		return ((arr[(length/2)]+arr[(length/2)+1])/2);
+	}
+	//odd
+	else
+	{
+		return (arr[(length/2)+1]);
+	}
 }
 
 unsigned char find_mean(unsigned char* arr,unsigned int length)
 {
+	unsigned int sum=0;
+	unsigned char counter;
+	
+	for(counter=0;counter<length;counter++)
+	{
+		sum+=arr[counter];
+	}
+	
+	return sum/length;
+
 }
 
 unsigned char find_maximum(unsigned char* arr,unsigned int length)
 {
+	unsigned char counter,max=arr[0];
+	
+	for(counter=1;counter<length;counter++)
+	{
+		if(arr[counter]>max)
+		{
+			max=arr[counter];
+		}
+	}
+	
+	return max;
 }
 
 
 unsigned char find_minimum(unsigned char* arr,unsigned int length)
 {
+	unsigned char counter,min=arr[0];
+	
+	for(counter=1;counter<length;counter++)
+	{
+		if(arr[counter]<min)
+		{
+			min=arr[counter];
+		}
+	}
+	
+	return min;
+
 }
 
 void sort_array(unsigned char* arr,unsigned int length)
 {
+	unsigned char i ,j,temp;
+	
+	for (i = 0; i < length; ++i) 
+    {
+		for (j = i + 1; j < length; ++j) 
+		{
+			if (arr[i] < arr[j]) 
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+    }
 }
